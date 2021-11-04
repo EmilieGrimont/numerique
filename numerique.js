a17730bce76705bed94b0aa081127a4b7ea34926
EmilieGrimont <emilie.grimont78@gmail.com>
2021-11-04 11:30:26 +0100

    final

--- numerique.js
+++ numerique.js
@@ -1,6 +1,7 @@
-let resultat = '1'
-for (let a = 0; a < 3; a++){
-
- console.log(a)   
+function suitNum(){
+let resultat = document.getElementById('nombre').value
+for (let a = 0; a <= resultat; a++){
+    document.getElementById('text').innerHTML +=a
 }
-
+}
+document.getElementById('valid').addEventListener('click',suitNum)
